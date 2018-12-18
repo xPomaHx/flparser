e07ea863fe271173ac5a591c0fa1a14e2d6da8a0
xPomaHx <gigoroma@yandex.ru>
2018-12-18 20:58:24 +0300

    regexp match

--- index.js
+++ index.js
@@ -40,58 +40,43 @@ let getActualId = async () => {
     })
     return id;
 };
-let getAllWords = function(str) {
-    console.dir(str);
-    var regprox = /[A-Za-zА-Яа-я]*/gm;
-    var rez = [];
-    var m;
-    do {
-        m = regprox.exec(str);
-        if (m && m[1] && m[2]) {
-            rez.push([m[1], m[2]]);
-        }
-    } while (m !== null)
-    return rez;
-};
 
-function matchAll(str, regex) {
-    var res = [];
-    var m;
-    if (regex.global) {
-        while (m = regex.exec(str)) {
-            res.push(m[1]);
-        }
-    } else {
-        if (m = regex.exec(str)) {
-            res.push(m[1]);
-        }
-    }
-    return res;
-}
 let getMetaData = (txt) => {
     txt = txt.replace("&#8211", "")
     txt = txt.replace("&nbsp", "")
     txt = txt.replace("&quot", "")
     txt = txt.toLowerCase();
+    let reg = {
+        telegram: /(telegram|телеграмм|телеграм)+[\s:\.,\n@]*([a-zA-z-0-9]{2,})|t\.me/mgi,
+        vk: /vk.com\/[A-Za-z0-9]+/mgi,
+        email: /([A-Za-z0-9_\-]+\.*[A-Za-z0-9_\-]+@[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9]\.+[A-Za-z]{2,4})/mgi,
+        skype: /(skype|скайп|скайпе|скайпу)+[\s:\(\)\-–\.,\n@]*?([a-zA-z0-9\.\-_+!@#$%^?>:<&*=]{3,})/mgi,
+    };
+    for (let regt in reg) {
+        reg[regt] = (txt.match(reg[regt]));
+    }
+    return reg
 };
 (async () => {
     //return;
     console.dir("start")
     console.time("start")
     await mongooseconnection;
-    let t = await getDataFromProjectPage(43);
-    //let t = await getActualId();
-    var bulk = Word.collection.initializeUnorderedBulkOp();
-    for (let w of t.content.toLowerCase().match(/[A-Za-zА-Яа-я]*/gm)) {
-        if (w && w.length > 1) {
-            bulk.find({
-                word: w,
-            }).upsert().updateOne({
-                $inc: { number: 1 }
-            });
-        }
-    }
-    await bulk.execute()
+    let aid = await getActualId();
+    let t = await getDataFromProjectPage(2170753);
+    console.dir(getMetaData(t.content));
+    // var bulk = Word.collection.initializeUnorderedBulkOp();
+    // for (let w of t.content.match(/[A-Za-zА-Яа-я]*/gim)) {
+    //     if (w && w.length > 1) {
+    //         bulk.find({
+    //             word: w,
+    //         }).upsert().updateOne({
+    //             $inc: { number: 1 }
+    //         });
+    //     }
+    // }
+    // await bulk.execute()
+
     console.timeEnd("start")
 })();
 
